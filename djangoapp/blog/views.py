aa4e9ef4ae38bd3a49312b199a6c95775b0de2ec
Lucas Flauzino <27flauzino@gmail.com>
2024-01-26 13:28:28 -0300

    layout do blog completo

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+
+posts = list(range(1000))
 
 
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/page.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/post.html',
+        {
+            'page_obj': page_obj,
+        }
     )
