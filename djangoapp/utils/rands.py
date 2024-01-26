886ebb91dce7fe2177794b78d791c9e9730fd250
Lucas Flauzino <27flauzino@gmail.com>
2024-01-26 14:48:09 -0300

    adicionando os models tag,categor,page e post

---
+++ djangoapp/utils/rands.py
@@ -0,0 +1,14 @@
+import string
+from random import SystemRandom
+from django.utils.text import slugify
+
+
+def random_letters(k=5):
+    return ''.join(SystemRandom().choices(
+        string.ascii_lowercase + string.digits, k=k
+        )
+    )
+
+
+def slug_new(text):
+    return slugify(text) + '-' + random_letters(5)
